899b0209be9f8cc1353e5a38c9ebcb92cede3165
Alexey Vasilenko <alexey.vasilenko@inslab.ru>
2021-06-07 19:03:24 +0300

    fix edit form layout, image paths,css formatting

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__container');
 const popupWindow = document.querySelector('.popup');
 
-editButton.addEventListener('click',showPopup);
-closeButton.addEventListener('click',hidePopup);
+editButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', hidePopup);
 saveButton.addEventListener('submit', formSubmitHandler);
 
-function showPopup(){
+function showPopup() {
   let profileTitle = document.querySelector('.profile__title');
   let profileSubTitle = document.querySelector('.profile__subtitle');
   let popupInputName = document.getElementById('name');
@@ -19,18 +19,18 @@ function showPopup(){
   popupWindow.classList.add('popup_opened');
 }
 
-function hidePopup(){
+function hidePopup() {
   popupWindow.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
-  let profileTitle =  document.querySelector('.profile__title');
+  let profileTitle = document.querySelector('.profile__title');
   let profileSubTitle = document.querySelector('.profile__subtitle');
   let popupInputName = document.getElementById('name');
   let popupInputdescription = document.getElementById('description');
-  profileTitle.textContent=popupInputName.value;
-  profileSubTitle.textContent=popupInputdescription.value;
+  profileTitle.textContent = popupInputName.value;
+  profileSubTitle.textContent = popupInputdescription.value;
   hidePopup();
 }
 
