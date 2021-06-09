9609ec0bfd2b534bac3fb715bad79b6aa40566c1
alexey.vasilenko <alexey.vasilenko@inslab.ru>
2021-06-09 09:14:34 +0300

    fix: index.js review variables order

--- js/index.js
+++ js/index.js
@@ -2,17 +2,13 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__container');
 const popupWindow = document.querySelector('.popup');
-
-editButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', hidePopup);
-saveButton.addEventListener('submit', formSubmitHandler);
+const profileTitle = document.querySelector('.profile__title');
+const profileSubTitle = document.querySelector('.profile__subtitle');
+let popupInputName = document.getElementById('name');
+let popupInputdescription = document.getElementById('description');
 
 function showPopup() {
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubTitle = document.querySelector('.profile__subtitle');
-  let popupInputName = document.getElementById('name');
-  let popupInputdescription = document.getElementById('description');
-
+  popupInputdescription = document.getElementById('description');
   popupInputName.value = profileTitle.textContent;
   popupInputdescription.value = profileSubTitle.textContent;
 
@@ -23,14 +19,17 @@ function hidePopup() {
   popupWindow.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function submitFormHandler(evt) {
   evt.preventDefault();
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubTitle = document.querySelector('.profile__subtitle');
-  let popupInputName = document.getElementById('name');
-  let popupInputdescription = document.getElementById('description');
+  
+  popupInputName = document.getElementById('name');
+  popupInputdescription = document.getElementById('description');
+  
   profileTitle.textContent = popupInputName.value;
   profileSubTitle.textContent = popupInputdescription.value;
   hidePopup();
 }
 
+editButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', hidePopup);
+saveButton.addEventListener('submit', submitFormHandler);	
