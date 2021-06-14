543175615be7509c705c287e17edd60b258ca2a6
Alexey Vasilenko <alexey.vasilenko@inslab.ru>
2021-06-14 21:16:38 +0300

    feat: JS load elements

--- js/index.js
+++ js/index.js
@@ -6,6 +6,50 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubTitle = document.querySelector('.profile__subtitle');
 let popupInputName = document.getElementById('name');
 let popupInputdescription = document.getElementById('description');
+const elementTemplate = document.querySelector('#element').content;
+
+const elementsContainer = document.querySelector('.elements');
+renderElements();
+
+function renderElements(){
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  initialCards.forEach(card => {
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    element.querySelector('.element__image').src=card.link;
+    element.querySelector('.element__image').alt=card.name;
+    element.querySelector('.element__title').textContent=card.name;
+    elementsContainer.append(element);
+  });
+
+
+
+}
 
 function showPopup() {
   popupInputName.value = profileTitle.textContent;
