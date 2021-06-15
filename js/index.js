d1b41ea8b7d109cf8780d09c3193b9ff481468ed
Alexey Vasilenko <alexey.vasilenko@inslab.ru>
2021-06-15 13:13:35 +0300

    feat add cards form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//элементы управления профилем
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__container');
@@ -6,12 +7,22 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubTitle = document.querySelector('.profile__subtitle');
 let popupInputName = document.getElementById('name');
 let popupInputdescription = document.getElementById('description');
-const elementTemplate = document.querySelector('#element').content;
 
+
+//элементы управления карточками мест
 const elementsContainer = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#element').content;
+const addElButton = document.querySelector('.profile__add-button');
+const closeElPopupButton = document.getElementById('close-element-popup');
+const addElPopup = document.getElementById('add-element');
+const saveElButton = document.getElementById('save-element');
+const elNameInput = document.getElementById('element-name');
+const elLinkInput = document.getElementById('element-link');
+
+//Шесть карточек «из коробки»
 renderElements();
 
-function renderElements(){
+function renderElements() {
   const initialCards = [
     {
       name: 'Архыз',
@@ -40,21 +51,15 @@ function renderElements(){
   ];
 
   initialCards.forEach(card => {
-    const element = elementTemplate.querySelector('.element').cloneNode(true);
-    element.querySelector('.element__image').src=card.link;
-    element.querySelector('.element__image').alt=card.name;
-    element.querySelector('.element__title').textContent=card.name;
-    elementsContainer.append(element);
+    addElement(card);
   });
-
-
 
 }
 
+//Форма редактирования профиля
 function showPopup() {
   popupInputName.value = profileTitle.textContent;
   popupInputdescription.value = profileSubTitle.textContent;
-
   popupWindow.classList.add('popup_opened');
 }
 
@@ -64,12 +69,41 @@ function hidePopup() {
 
 function submitFormHandler(evt) {
   evt.preventDefault();
-
   profileTitle.textContent = popupInputName.value;
   profileSubTitle.textContent = popupInputdescription.value;
   hidePopup();
 }
 
+//Функции работы с формой карточек мест
+function showAddElPopup() {
+  elNameInput.value = '';
+  elLinkInput.value = '';
+  addElPopup.classList.add('popup_opened');
+}
+function hideAddElPopup() {
+  addElPopup.classList.remove('popup_opened');
+}
+
+function submitElFormHandler(evt) {
+  evt.preventDefault();
+  let card = { name: elNameInput.value, link: elLinkInput.value };
+  addElement(card);
+}
+
+function addElement(card) {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = card.link;
+  element.querySelector('.element__image').alt = card.name;
+  element.querySelector('.element__title').textContent = card.name;
+  elementsContainer.append(element);
+  hideAddElPopup();
+}
+
+//Листенеры кнопок
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', hidePopup);
 saveButton.addEventListener('submit', submitFormHandler);
+saveElButton.addEventListener('click', submitElFormHandler);
+addElButton.addEventListener('click', showAddElPopup);
+closeElPopupButton.addEventListener('click', hideAddElPopup);
+
