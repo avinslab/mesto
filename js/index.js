b03123b68cb293db44c986bfa00318606929ef31
Alexey Vasilenko <alexey.vasilenko@inslab.ru>
2021-06-16 17:42:37 +0300

    fix: index.js functions and variables by review

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
 //элементы управления профилем
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
+const closeButton = document.querySelector('#close-profile-popup');
 const saveButton = document.querySelector('.popup__container');
-const popupWindow = document.querySelector('.popup');
+const profilePopup = document.querySelector('#edit-profile');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubTitle = document.querySelector('.profile__subtitle');
-let popupInputName = document.getElementById('name');
-let popupInputdescription = document.getElementById('description');
+const popupInputName = document.querySelector('#name');
+const popupInputdescription = document.querySelector('#description');
 
 //элементы управления карточками мест
 const elementsContainer = document.querySelector('.elements');
 const elementTemplate = document.querySelector('#element').content;
 const addElButton = document.querySelector('.profile__add-button');
-const closeElPopupButton = document.getElementById('close-element-popup');
-const addElPopup = document.getElementById('add-element');
-const saveElButton = document.getElementById('save-element');
-const elNameInput = document.getElementById('element-name');
-const elLinkInput = document.getElementById('element-link');
+const closeElPopupButton = document.querySelector('#close-element-popup');
+const addElPopup = document.querySelector('#add-element');
+const saveElButton = document.querySelector('#save-element');
+const elNameInput = document.querySelector('#element-name');
+const elLinkInput = document.querySelector('#element-link');
+const formAddCard = document.querySelector('#add-element-form');
 
 //элементы управления попапа с картинкой
-const imagePopup = document.getElementById('image-popup');
-const closeImagePopupButton = document.getElementById('close-image-popup');
+const imagePopup = document.querySelector('#image-popup');
+const closeImagePopupButton = document.querySelector('#close-image-popup');
 const imagePopupPicture = document.querySelector('.popup__image-preview');
 const imagePopupTitle = document.querySelector('.popup__image-title');
 
 //Шесть карточек «из коробки»
-renderElements();
 
 function renderElements() {
   const initialCards = [
@@ -61,43 +61,44 @@ function renderElements() {
 
 }
 
-//Форма редактирования профиля
-
-function showPopup() {
-  popupInputName.value = profileTitle.textContent;
-  popupInputdescription.value = profileSubTitle.textContent;
-  popupWindow.classList.add('popup_opened');
+//Управление видимостью попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened'); //добавляем к popup класс popup_opened
+}
+function closePopup(popup) {
+  popup.classList.remove('popup_opened'); //удаляем у popup класс popup_opened
 }
 
-function hidePopup() {
-  popupWindow.classList.remove('popup_opened');
+//Форма редактирования профиля
+
+function showProfilePopup() {
+  popupInputName.value = profileTitle.textContent;
+  popupInputdescription.value = profileSubTitle.textContent;
+  openPopup(profilePopup);
 }
 
 function submitFormHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = popupInputName.value;
   profileSubTitle.textContent = popupInputdescription.value;
-  hidePopup();
+  closePopup(profilePopup);
 }
 
 //Функции работы с формой карточек мест
 
 function showAddElPopup() {
-  elNameInput.value = '';
-  elLinkInput.value = '';
-  addElPopup.classList.add('popup_opened');
-}
-function hideAddElPopup() {
-  addElPopup.classList.remove('popup_opened');
+  formAddCard.reset();
+  openPopup(addElPopup);
 }
 
 function submitElFormHandler(evt) {
   evt.preventDefault();
-  let card = { name: elNameInput.value, link: elLinkInput.value };
+  const card = { name: elNameInput.value, link: elLinkInput.value };
   addElement(card);
+  closePopup(addElPopup);
 }
 
-function addElement(card) {
+function createCard(card) {
   const element = elementTemplate.querySelector('.element').cloneNode(true);
   element.querySelector('.element__image').src = card.link;
   element.querySelector('.element__image').alt = card.name;
@@ -105,29 +106,29 @@ function addElement(card) {
   element.querySelector('.element__like-button').addEventListener('click', function (evt) { evt.target.classList.toggle('element__like-button_active'); });
   element.querySelector('.element__trash-button').addEventListener('click', function (evt) { evt.target.parentElement.remove(); });
   element.querySelector('.element__image').addEventListener('click', function (evt) { showImagePopup(evt); });
-  elementsContainer.append(element);
-  hideAddElPopup();
+  return element;
+}
+
+function addElement(element) {
+  elementsContainer.prepend(createCard(element));
 }
 
 //Функции работы попапа с картинкой
 
 function showImagePopup(evt) {
-  console.log(evt.target);
   imagePopupPicture.src = evt.target.src;
   imagePopupPicture.alt = evt.target.alt;
   imagePopupTitle.textContent = evt.target.alt;
-  imagePopup.classList.add('popup_opened');
-}
-function hideImagePopup(evt) {
-  imagePopup.classList.remove('popup_opened');
+  openPopup(imagePopup);
 }
 
 //Листенеры кнопок
-editButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', hidePopup);
+editButton.addEventListener('click', showProfilePopup);
+closeButton.addEventListener('click', function () { closePopup(profilePopup) });
 saveButton.addEventListener('submit', submitFormHandler);
 saveElButton.addEventListener('click', submitElFormHandler);
 addElButton.addEventListener('click', showAddElPopup);
-closeElPopupButton.addEventListener('click', hideAddElPopup);
-closeImagePopupButton.addEventListener('click', hideImagePopup);
+closeElPopupButton.addEventListener('click', function () { closePopup(addElPopup) });
+closeImagePopupButton.addEventListener('click', function () { closePopup(imagePopup) });
 
+renderElements();
